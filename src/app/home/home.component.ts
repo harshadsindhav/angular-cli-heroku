2d37a48c797f8481e88f00316566a6020072e665
harshadsindhav <harshad.sindhav@gmail.com>
2018-05-31 11:49:31 +0530

    simulator change added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,14 +8,23 @@ import { Router, ActivatedRoute, Params} from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  @Input() returnUrl : string;
-
+  returnUrl: string;
+  decision_code: string;
+  transaction_id: string;
+  currency: string;
+  amount: string;
+  error_mesg: string;
 
   constructor(private router : Router, private routes : ActivatedRoute) {
 
     this.routes.queryParams.subscribe((params : Params) => {
         if(params['return_url'] != undefined) {
               this.returnUrl = params['return_url'];
+              this.decision_code = params['decision_code'];
+              this.transaction_id = params['transaction_id'];
+              this.currency = params['currency'];
+              this.amount = params['amount'];
+              this.error_mesg = params['error_mesg'];
         }
     });
   }
@@ -25,7 +34,7 @@ export class HomeComponent implements OnInit {
 
   submitOrder() {
     console.log("Redirecting to " + this.returnUrl);
-    window.location.href = this.returnUrl;
+    window.location.href = this.returnUrl + "&decision_code=" + this.decision_code + "&transaction_id=" + this.transaction_id + "&currency=" + this.currency + "&amount=" + this.amount + "&error_mesg=" + this.error_mesg;
 }
 
 }
